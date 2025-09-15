df64d9ca4c76a88a37dc25b24ab66e4d05fe7cfa
tinchicus <126396777+tinchicus@users.noreply.github.com>
2025-09-15 13:38:24 -0300

    Delete args/v1 directory

--- args/v1/Args.java
+++
@@ -1,8 +0,0 @@
-public class Args
-{
-  public static void main(String[] args)
-  {
-     for(int i=0, j=1; i < args.length; i++, j++)
-        System.out.println("arg #" + j + ": " + args[i]);
-  }
-}
