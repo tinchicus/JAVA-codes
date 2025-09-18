78b09c73dd305f6c5e65468507b16506c497caed
tinchicus <126396777+tinchicus@users.noreply.github.com>
2025-09-18 15:49:23 -0300

    Create fileStreamTest.java

---
+++ Archivos-ES/fileStreamTest.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class fileStreamTest
+{
+
+        public static void main(String[] args)
+        {
+
+        try
+        {
+                byte bWrite[] = { 11,21,3,40,5 };
+                OutputStream os = new FileOutputStream("prueba.txt");
+                for(int x = 0; x < bWrite.length; x++)
+                        os.write( bWrite[x] );
+                os.close();
+
+                InputStream is = new FileInputStream("prueba.txt");
+                int tamano = is.available();
+
+                for(int i = 0; i < tamano; i++)
+                        System.out.print((char)is.read() + "  ");
+                System.out.print("\n");
+                is.close();
+        }
+
+        catch(IOException e)
+        {
+                System.out.print("Excepcion");
+        }
+        }
+}
